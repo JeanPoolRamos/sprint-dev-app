c83c7a70ab1112942cbf4fd3846ca3da89cd9333
Jean Ramos <jean.ramos@grupopromerica.com>
2023-04-18 18:07:13 -0600

    Primer commit

---
+++ src/main/java/com/pfcti/com/print/dev/app/PrintDevAppApplication.java
@@ -0,0 +1,13 @@
+package com.pfcti.com.print.dev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintDevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintDevAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfcti/com/print/dev/app/PrintDevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.com.print.dev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintDevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
